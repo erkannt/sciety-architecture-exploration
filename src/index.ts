c2dd146a4db719b4d0f5e608fa786df7582149e9
Daniel Haarhoff <daniel@rknt.de>
2022-11-10 14:39:06 +0000

    How pages are mapped to routes is a concern of the app, not the pages

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { App } from '../types';
 import { pages } from './pages';
 
+const pageRepresentations = [
+	{path: '/', page: pages.home},
+	{path: '/search', page: pages.search}
+]
+
 const app: App = ({
-	pages
+	pages: pageRepresentations
 })
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,10 +1,10 @@
-import { PageRepresentation } from '../../types';
 import { home, Queries as HomeQueries } from './home';
-import { search, Queries as SearchQueries } from './search';
+import { Queries as SearchQueries, search } from './search';
 
-export type Queries = HomeQueries & SearchQueries
+export type Queries = HomeQueries
+	& SearchQueries
 
-export const pages: ReadonlyArray<PageRepresentation> = [
-	{path: '/', page: home},
-	{path: '/search', page: search}
-]
+export const pages = {
+	home,
+	search
+}
